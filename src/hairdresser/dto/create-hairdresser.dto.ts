13c050ac719aea0c05217b7717806ba5a3f4a733
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-18 19:56:45 -0300

    DTO create hairdresser

--- src/hairdresser/dto/create-hairdresser.dto.ts
+++ src/hairdresser/dto/create-hairdresser.dto.ts
@@ -1,1 +1,48 @@
-export class CreateHairdresserDto {}
+import { Transform } from 'class-transformer';
+import {
+  Contains,
+  IsEmail,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateHairdresserDto {
+  @Transform(({ value }: { value: string }) => value.trim())
+  @IsString()
+  @MinLength(5, {
+    message: 'Name is too short. Min length is 5 characters',
+  })
+  @MaxLength(15, {
+    message: 'Name is too long. Max length is 15 characters',
+  })
+  name: string;
+
+  @Transform(({ value }: { value: string }) => value.trim())
+  @IsString()
+  @MinLength(3)
+  @MaxLength(12)
+  nickname: string;
+
+  @Transform(({ value }: { value: string }) => value.trim())
+  @IsEmail(
+    {},
+    {
+      message: 'Invalid email address',
+    },
+  )
+  email: string;
+
+  @Transform(({ value }: { value: string }) => value.trim())
+  @IsString()
+  @MinLength(8, {
+    message: 'Password is too short. Min length is 8 characters',
+  })
+  @MaxLength(10, {
+    message: 'Password is too long. Max length is 10 characters',
+  })
+  @Contains(' ', {
+    message: 'Password must contain at least one space',
+  })
+  password: string;
+}
--- src/hairdresser/entities/hairdresser.entity.ts
+++ src/hairdresser/entities/hairdresser.entity.ts
@@ -35,7 +35,7 @@ export class Hairdresser {
   @Column({
     type: 'set',
     enum: Type,
-    default: [],
+    default: [Type.CUT],
   })
   type: Type[];
 
