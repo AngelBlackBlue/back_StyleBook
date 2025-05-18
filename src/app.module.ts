5aae9ecb522ae9dc07196b73bf2813ccdb25753b
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-18 16:34:35 -0300

    create calendar

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { HairdresserModule } from './hairdresser/hairdresser.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, HairdresserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, HairdresserModule, CalendarModule],
   controllers: [],
   providers: [],
 })
